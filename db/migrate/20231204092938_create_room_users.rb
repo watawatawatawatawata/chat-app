f17c2f4e45637ba2ceea96e2affdbfc11aa2b650
watawatawatawatawata <rstoyo.kazuki.watanabe@gmail.com>
2023-12-05 14:20:46 +0900

    プルダウンの実装

--- db/migrate/20231204092938_create_room_users.rb
+++ db/migrate/20231204092938_create_room_users.rb
@@ -2,7 +2,7 @@ class CreateRoomUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :room_users do |t|
       t.references :user,   null: false, foreign_key: true
-      t.references :rooms,  null: false, foreign_key: true
+      t.references :room,   null: false, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_04_090212) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_04_092938) do
   create_table "room_users", charset: "utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
-    t.bigint "rooms_id", null: false
+    t.bigint "room_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["rooms_id"], name: "index_room_users_on_rooms_id"
+    t.index ["room_id"], name: "index_room_users_on_room_id"
     t.index ["user_id"], name: "index_room_users_on_user_id"
   end
 
@@ -39,6 +39,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_04_090212) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "room_users", "rooms", column: "rooms_id"
+  add_foreign_key "room_users", "rooms"
   add_foreign_key "room_users", "users"
 end
